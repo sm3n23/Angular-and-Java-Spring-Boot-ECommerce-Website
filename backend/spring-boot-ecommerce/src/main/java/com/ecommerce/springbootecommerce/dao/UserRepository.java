f178428b9de18baa632f95c1c8440ca2c51083b9
sm3n23 <121973133+sm3n23@users.noreply.github.com>
2023-08-23 10:42:37 +0100

    new template

--- backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/dao/UserRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/dao/UserRepository.java
@@ -3,9 +3,14 @@ package com.ecommerce.springbootecommerce.dao;
 import com.ecommerce.springbootecommerce.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    Optional<User> findByUsernameOrEmail(String usernameOrEmail, String usernameOrEmail1);
     Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
